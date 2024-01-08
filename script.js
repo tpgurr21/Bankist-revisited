3ed151858cb0e2964f81994ce0038a4c06e69e27
Thomas Gurr <tpgurr21@gmail.com>
2024-01-07 22:50:10 -0500

    Reduce method

--- script.js
+++ script.js
@@ -184,3 +184,13 @@ console.log(depositsFor);
 const withdrawals = movements.filter(mov => mov < 0);
 
 console.log(withdrawals);
+
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
