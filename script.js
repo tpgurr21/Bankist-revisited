edcef61fb6d6e52c5f496e6a41cd3343b3b81aea
Thomas Gurr <tpgurr21@gmail.com>
2024-01-11 14:25:22 -0500

    Went over the every, some and includes methods

--- script.js
+++ script.js
@@ -166,6 +166,21 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount)
+  }
+  inputLoanAmount.value = '';
+});
+
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
@@ -432,7 +447,7 @@ const avg1 = calcAverageHumanAge(dogAges1);
 const avg2 = calcAverageHumanAge(dogAges2);
 console.log(avg1, avg2);
 
-*/
+
 
 const firstWithdrawal = movements.find(mov => mov < 0);
 console.log(movements);
@@ -442,3 +457,26 @@ console.log(accounts);
 
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
+
+*/
+
+//  INCLUDES: EQUALITY
+console.log(movements);
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+// .some basically means "any" because if anything is true, it'll return true
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+//  EVERY
+console.log(movements.every(mov => mov > 0))
+console.log(account4.movements.every(mov => mov > 0))
+
+//  Separate callback
+const deposit = mov => mov > 0
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
